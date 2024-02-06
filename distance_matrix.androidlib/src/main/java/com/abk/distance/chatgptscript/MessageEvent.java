68dece674cd4f0f58c67e39a3bcb27298d614d0a
CCE1312 <cheamchungen@gmail.com>
2024-02-06 18:00:48 +0800

    upload

--- distance_matrix.androidlib/src/main/java/com/abk/distance/chatgptscript/MessageEvent.java
+++
@@ -1,8 +0,0 @@
-package com.abk.distance.chatgptscript;
-
-public class MessageEvent {
-    public final String message;
-    public MessageEvent(String message) {
-        this.message = message;
-    }
-}
